d68b3c7732c60e9eea43da52cab0ba20ab048d8c
Nicolai Weitkemper <kontakt@nicolaiweitkemper.de>
2021-04-30 13:14:07 +0200

    Verbesserungen

--- Präsenzübungsblatt 1/Aufgabe_1_a.py
+++ Präsenzübungsblatt 1/Aufgabe_1_a.py
@@ -3,15 +3,16 @@ import numpy as np
 
 groesse, gewicht = np.genfromtxt('Groesse_Gewicht.txt', unpack=True)
 
-fig, axs = plt.subplots(2,3)
 
 mat = [
 [ 5, 10, 15],
 [20, 30, 50],
 ]
 
-for i, rows in enumerate(mat):
-    print(i, rows)
-    for k, bins in enumerate(rows):
-        axs[i, k].hist(groesse, bins=bins)
+for daten, name in [(groesse, 'Größe'), (gewicht, 'Gewicht')]:
+    fig, axs = plt.subplots(2,3)
+    fig.suptitle(name)
+    for i, rows in enumerate(mat):
+        for k, bins in enumerate(rows):
+            axs[i, k].hist(daten, bins=bins)
 plt.show()
